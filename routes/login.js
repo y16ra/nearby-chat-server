280025fc80c9ae86d0dd7741d532172b94445d1a
y16ra <yuichi.ichimura@G2014-049.local>
2014-12-16 16:04:23 +0900

    improve login flow

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
+	// already logined.
+	if (req.user) {
+		return res.redirect('/loby');
+	}
 	// ログインページ表示
 	res.render('login');
 });
